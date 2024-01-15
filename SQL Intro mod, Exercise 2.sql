f1191dc21f1e1dc08cc8b35641e08086b6dc27f3
CLochstampfor60 <cflochjr@gmail.com>
2024-01-15 13:30:42 -0500

    exercise complete

--- SQL Intro mod, Exercise 2.sql
+++ SQL Intro mod, Exercise 2.sql
@@ -1,60 +1,59 @@
 -- Using the bestbuy database: Copy the SQL Comments below and paste them into your MySql Workbench.
 -- find all products
-Select * From Products;
+Select * From bestbuy.products;
 
 -- find all products that cost $1400
-Select * from Products
-where Price = 1400;
+Select * from bestbuy.products
+where products.Price = 1400.00;
 
 -- find all products that cost $11.99 or $13.99
-Select * from Products
-where Price = 11.99 or 13.99;
+Select * from bestbuy.products as p
+where p.Price = 11.99 or 13.99;
 
 -- find all products that do NOT cost 11.99 - using NOT
-Select * from Products
-where NOT Price = 11.99;
+Select * from bestbuy.products as p
+where NOT p.Price = 11.99;
 
 -- find all products and sort them by price from greatest to least
-Select * from Products
-Order By Price desc;
+Select * from bestbuy.products as p
+Order By p.Price desc;
 
 -- find all employees who don't have a middle initial
-Select * from Employees
+Select * from bestbuy.employees
 Where MiddleInitial = null;
 
 -- find distinct product prices
-Select Distinct Price
-From Products;
+Select Distinct bestbuy.products.price as t
+From t.products;
 
 -- find all employees whose first name starts with the letter ‘j’
-Select * from Employees
+Select * from bestbuy.employees
 Where FirstName LIKE 'j%';
 
 -- find all Macbooks
-Select * from Products
-Where Name = "Macbooks";
+Select * from bestbuy.Products
+Where Name = "Macbook";
 
 -- find all products that are on sale
-Select * from Products
-Where OnSale;
+Select * from bestbuy.Products
+Where OnSale = 1;
 
 -- find the average price of all products
-Select AVG(Price) from Products;
+Select AVG(products.price) from bestbuy.products;
 
 -- find all Geek Squad employees who don't have a middle initial
 -- Select * From Employees("Title.Geek Squad")
 -- Where MiddleInitial = Null;
 
-Select Title.GeekSquad
-from Employees
-where MiddleInitial = Null;
+Select * FROM bestbuy.employees as e
+where e.MiddleInitial = Null AND e.title ="Geek Squad";
 
 
 -- find all products from the products table whose stock level is in the range -- of 500 to 1200. 
 -- Order by Price from least to greatest. Hint: Use the between keyword
-Select * From Products
+Select * From bestbuy.Products
 Where StockLevel Between 500 and 1200
-Order By Price desc;
+Order By Price;
 
 
 
